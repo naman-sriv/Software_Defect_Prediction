f4e19d25cb690ffc0b2558c0a0738c168a0506bb
Naman <namansriv94@gmail.com>
2023-12-03 20:13:00 +0000

    app changes

--- app.py
+++ app.py
@@ -22,5 +22,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = np.array(data).reshape(1,-1)
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "The defect status is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
